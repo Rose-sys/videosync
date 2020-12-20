938ee1eea91291895612d5a55134075bd506cc1a
Rose-sys <76407061+Rose-sys@users.noreply.github.com>
2020-12-20 21:16:08 +0100

    Update createpassword.php

--- createpassword.php
+++ createpassword.php
@@ -3,6 +3,6 @@ $hash_variable_salt = password_hash($_GET['pw'],
         PASSWORD_DEFAULT, array('cost' => 12));
 
 echo "HASH: ".$hash_variable_salt."<br>";
-echo "Verify (should return 1): ". password_verify($argv[1],
+echo "Verify (should return 1): ". password_verify($_GET['pw'],
     $hash_variable_salt ) . "<br>";
     
